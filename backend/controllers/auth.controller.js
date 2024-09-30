778ac0fde53152d6182ae48b1e4f6e2021ea231f
Parth Bhuva <parth.d.bhuva2331@gmail.com>
2024-10-01 05:06:10 +0530

    add data to database and cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,80 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateTokanAndSetcode } from "../utils/generateToken.js";
+
 export async function signup(req, res) {
-    res.send("Signup route")
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are requrired" });
+    }
+
+    const emailRegex =
+      /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ success: false, message: "Invalid email" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "password must be at least 6 characters",
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email });
+
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
+    }
+
+    const existingUserByUsername = await User.findOne({ username: username });
+
+    if (existingUserByUsername) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already exists" });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const PORFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
+
+    const image = PORFILE_PICS[Math.floor(Math.random() * PORFILE_PICS.length)];
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      username,
+      image,
+    });
+
+    generateTokanAndSetcode(newUser._id, res);
+
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      user: {
+        ...newUser._doc,
+        password: "",
+      },
+    });
+  } catch (error) {
+    console.log("Error in signup controllar", error.message);
+    res.status(500).json({ success: false, message: "internal server error" });
+  }
 }
 export async function login(req, res) {
-    res.send("Login route")
+  res.send("Login route");
 }
 export async function logout(req, res) {
-    res.send("Logout route")
+  res.send("Logout route");
 }
