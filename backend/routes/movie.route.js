73d2072b22738c24670c48a4da1448bf78c0f924
Parth Bhuva <parth.d.bhuva2331@gmail.com>
2024-10-01 08:29:33 +0530

    add many movie api

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { getTrendingMovie } from "../controllers/movie.controller.js";
-
-
+import { getTrendingMovie, getMovieTrailers, getMovieDetails, getMovieSimilar, getMovieCategory } from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
-router.get("/trending", getTrendingMovie)
+router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getMovieSimilar);
+router.get("/:category", getMovieCategory);
 
 export default  router;
