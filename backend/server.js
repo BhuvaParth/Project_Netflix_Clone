8a904b5e031d3d498ec483b5d44aa69ac68fac98
Parth Bhuva <parth.d.bhuva2331@gmail.com>
2024-10-01 08:02:57 +0530

    add trending movie api

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 // const express = require('express')
-import express from 'express';
-import authRoutes from "./routes/auth.route.js"
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
 import dotenv from "dotenv";
-import { ENV_VAR } from './config/envVars.js';
-import { connectDB } from './config/db.js';
+import { ENV_VAR } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const port = ENV_VAR.PORT;
 
 app.use(express.json());
-app.use("/api/v1/auth", authRoutes)
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(port, () => {
-    console.log("server started at " + port);  
-    connectDB();
-})
+  console.log("server started at " + port);
+  connectDB();
+});
+
