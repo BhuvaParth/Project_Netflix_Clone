bd65767a567f05c15af4257094143a3dc60aa891
Parth Bhuva <parth.d.bhuva2331@gmail.com>
2024-10-01 22:10:18 +0530

    change in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signuppage" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
